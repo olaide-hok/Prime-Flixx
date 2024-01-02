c81ab4fad380059d544600efd75c41544c628c32
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-02 10:09:02 +0100

    Add Search function and custom alert.
    Add search and api object to global object.
    
    Create search Fn which is invoked in the search page.
    
    Create searchAPIData Fn to make a GET request to TMDB search endpoint.
    
    Create a showAlert utility Fn.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
     currentPage: window.location.pathname,
+    search: {
+        term: '',
+        type: '',
+        page: 1,
+        totalPages: 1,
+    },
+    api: {
+        apiKey: '3419972f7a01d59a729595c7836782b3',
+        apiUrl: 'https://api.themoviedb.org/3/',
+    },
 }
 
 // Displays 20 most popular movies to the home page.
@@ -227,6 +237,21 @@ function displayBackgroundImage(type, backgroundPath) {
     }
 }
 
+// Search Movies/Shows
+async function search() {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+
+    global.search.type = urlParams.get('type')
+    global.search.term = urlParams.get('search-term')
+
+    if (global.search.term !== '' && global.search.term !== null) {
+        const results = await searchAPIData()
+    } else {
+        showAlert('Please enter a search term')
+    }
+}
+
 // Display Slider Movies
 async function displaySlider() {
     const {results} = await fetchAPIData('movie/now_playing')
@@ -279,13 +304,31 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-    const API_KEY = '3419972f7a01d59a729595c7836782b3'
-    const API_URL = 'https://api.themoviedb.org/3/'
+    const API_KEY = global.api.apiKey
+    const API_URL = global.api.apiUrl
 
     showSpinner()
 
     const response = await fetch(
         `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+    )
+
+    const data = await response.json()
+
+    hideSpinner()
+
+    return data
+}
+
+// Make search request to TMDB Seearch API
+async function searchAPIData() {
+    const API_KEY = global.api.apiKey
+    const API_URL = global.api.apiUrl
+
+    showSpinner()
+
+    const response = await fetch(
+        `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
     )
 
     const data = await response.json()
@@ -315,6 +358,16 @@ function highlightActiveLink() {
     })
 }
 
+// Show Alert
+function showAlert(message, className) {
+    const alertEl = document.createElement('div')
+    alertEl.classList.add('alert', className)
+    alertEl.appendChild(document.createTextNode(message))
+    document.querySelector('#alert').appendChild(alertEl)
+
+    setTimeout(() => alertEl.remove(), 3000)
+}
+
 // Add commas to numbers
 function addCommasToNumber(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
@@ -338,7 +391,7 @@ function init() {
             displayShowDetails()
             break
         case '/search.html':
-            console.log('Search')
+            search()
             break
     }
 
